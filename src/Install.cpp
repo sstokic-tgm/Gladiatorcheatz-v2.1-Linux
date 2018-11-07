7483b51cafcfb724c6f6fa2bd74dd4582cedf480
sstokic-tgm <stokic95@yahoo.com>
2018-11-07 16:10:29 +0100

    nearly all interfaces are retrived now, a few are still missing..

--- src/Install.cpp
+++ src/Install.cpp
@@ -33,7 +33,49 @@ ILocalize               *g_Localize = nullptr;
 
 void Installer::installGladiator()
 {
+    g_EngineClient = IfaceMngr::Get().getIface<IVEngineClient>("./bin/linux64/engine_client.so", "VEngineClient");
+    g_CHLClient = IfaceMngr::Get().getIface<IBaseClientDLL>("./csgo/bin/linux64/client_panorama_client.so", "VClient0");
+    g_EntityList = IfaceMngr::Get().getIface<IClientEntityList>("./csgo/bin/linux64/client_panorama_client.so", "VClientEntityList");
+    g_EngineTrace = IfaceMngr::Get().getIface<IEngineTrace>("./bin/linux64/engine_client.so", "EngineTraceClient");
+    g_CVar = IfaceMngr::Get().getIface<ICvar>("./bin/linux64/materialsystem_client.so", "VEngineCvar");
+    g_VGuiPanel = IfaceMngr::Get().getIface<IPanel>("./bin/linux64/vgui2_client.so", "VGUI_Panel");
+    g_DebugOverlay = IfaceMngr::Get().getIface<IVDebugOverlay>("./bin/linux64/engine_client.so", "VDebugOverlay");
+    g_VGuiSurface = IfaceMngr::Get().getIface<ISurface>("./bin/linux64/vguimatsurface_client.so", "VGUI_Surface");
+    g_MdlInfo = IfaceMngr::Get().getIface<IVModelInfoClient>("./bin/linux64/engine_client.so", "VModelInfoClient");
+    g_MdlRender = IfaceMngr::Get().getIface<IVModelRender>("./bin/linux64/engine_client.so", "VEngineModel");
+    g_RenderView = IfaceMngr::Get().getIface<IVRenderView>("./bin/linux64/engine_client.so", "VEngineRenderView");
+    g_MatSystem = IfaceMngr::Get().getIface<IMaterialSystem>("./bin/linux64/materialsystem_client.so", "VMaterialSystem");
+    g_GameEvents = IfaceMngr::Get().getIface<IGameEventManager2>("./bin/linux64/engine_client.so", "GAMEEVENTSMANAGER002", true);
+    g_MdlCache = IfaceMngr::Get().getIface<IMDLCache>("./bin/linux64/datacache_client.so", "MDLCache");
+    g_Prediction = IfaceMngr::Get().getIface<IPrediction>("./csgo/bin/linux64/client_panorama_client.so", "VClientPrediction");
+    g_GameMovement = IfaceMngr::Get().getIface<CGameMovement>("./csgo/bin/linux64/client_panorama_client.so", "GameMovement");
+    g_EngineSound = IfaceMngr::Get().getIface<IEngineSound>("./bin/linux64/engine_client.so", "IEngineSoundClient");
+    g_PhysSurface = IfaceMngr::Get().getIface<IPhysicsSurfaceProps>("./bin/linux64/vphysics_client.so", "VPhysicsSurfaceProps");
+    g_InputSystem = IfaceMngr::Get().getIface<IInputSystem>("./bin/linux64/inputsystem_client.so", "InputSystemVersion");
+    g_InputInternal = IfaceMngr::Get().getIface<uintptr_t>("./bin/linux64/vgui2_client.so", "VGUI_InputInternal");
+    g_Localize = IfaceMngr::Get().getIface<ILocalize>("./bin/linux64/localize_client.so", "Localize_");
 
+    void *library = dlopen("./csgo/bin/linux64/client_panorama_client.so", RTLD_NOLOAD | RTLD_NOW | RTLD_LOCAL);
+    if(library)
+    {
+        void *memAllocFn = dlsym(library, "g_pMemAlloc");
+        if(!memAllocFn)
+            dlclose(library);
+
+        dlclose(library);
+
+        g_pMemAlloc = *reinterpret_cast<IMemAlloc**>(memAllocFn);
+    }
+
+    // g_GlobalVars =
+    // g_ClientMode =
+    // g_ClientState =
+    // g_GlowObjManager =
+    // g_MoveHelper =
+    // g_RenderBeams =
+    // g_Input =
+
+    NetMngr::Get().init();
 }
 
 void Installer::uninstallGladiator()
