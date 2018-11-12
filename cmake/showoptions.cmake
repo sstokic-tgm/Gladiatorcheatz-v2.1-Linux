3eebc551b0fdb0657c33f11e0103eb1b5f0fc635
sstokic-tgm <stokic95@yahoo.com>
2018-11-12 03:55:46 +0100

    adjusted some formatting, to have a nice output rofl

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -1,14 +1,14 @@
 # output generic information about the core and buildtype chosen
 message("")
-message("* Gladiatorcheatz revision   : ${rev_hash} ${rev_date} (${rev_branch} branch)")
+message("* Gladiatorcheatz revision     : ${rev_hash} ${rev_date} (${rev_branch} branch)")
 if(UNIX)
-    message("* Gladiatorcheatz buildtype  : ${CMAKE_BUILD_TYPE}")
+    message("* Gladiatorcheatz buildtype    : ${CMAKE_BUILD_TYPE}")
 endif()
 
 if(WITHOUT_GIT)
-    message("* Use GIT revision hash  : No")
+    message("* Use GIT revision hash        : No")
 else()
-    message("* Use GIT revision hash  : Yes (default)")
+    message("* Use GIT revision hash        : Yes (default)")
 endif()
 
 message("")
