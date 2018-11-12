82f7a71373d228768e6fe0179350e26a932f5aba
sstokic-tgm <stokic95@yahoo.com>
2018-11-12 03:52:17 +0100

    updated PaintTraverse hook to match the func prototype and added simple output to see if everything is ok for now

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -3,28 +3,28 @@
 //
 
 #include "Gamehooking.h"
+#include "GitRevision.h"
 
-void Hooks::PaintTraverse(void* thisptr, void*, unsigned int panel, bool forceRepaint, bool allowForce)
+void Hooks::PaintTraverse(void* thisptr, unsigned int panel, bool forceRepaint, bool allowForce)
 {
-    static uint32_t HudZoomPanel;
-
-    if (!HudZoomPanel)
-    {
-        if (!strcmp("HudZoom", g_VGuiPanel->GetName(panel)))
-            HudZoomPanel = panel;
-    }
-
     o_PaintTraverse(thisptr, panel, forceRepaint, allowForce);
 
     static uint32_t FocusOverlayPanel;
     if (!FocusOverlayPanel)
     {
         if (strstr(g_VGuiPanel->GetName(panel), "MatSystemTopPanel"))
-            FocusOverlayPanel = panel;
+            {
+                FocusOverlayPanel = panel;
+
+                g_EngineClient->ExecuteClientCmd("clear");
+                g_CVar->ConsoleColorPrintf(Color(0, 153, 204, 255), "%s\n", GitRevision::GetFullVersion());
+                g_EngineClient->ExecuteClientCmd("version");
+                g_EngineClient->ExecuteClientCmd("toggleconsole");
+            }
     }
 
     if (panel == FocusOverlayPanel)
     {
-
+        
     }
 }
